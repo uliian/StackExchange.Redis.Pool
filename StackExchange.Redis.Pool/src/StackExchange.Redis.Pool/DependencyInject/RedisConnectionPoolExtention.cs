4f351615563e8ce86f6f9bc5c64037cce3b937ce
uliian <yncxcxz@gmail.com>
2017-10-17 23:15:15 +0800

    add impl

---
+++ StackExchange.Redis.Pool/src/StackExchange.Redis.Pool/DependencyInject/RedisConnectionPoolExtention.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeProject.ObjectPool;
+
+namespace StackExchange.Redis.Pool.DependencyInject
+{
+    public static class RedisConnectionPoolExtention
+    {
+        public static void AddRedisConnectionPool(ConfigurationOptions config,int poolSize)
+        {
+            new ObjectPool<PooledConnectionMultiplexer>(poolSize, () => new PooledConnectionMultiplexer(config));
+        }
+    }
+}
---
+++ StackExchange.Redis.Pool/src/StackExchange.Redis.Pool/PooledConnectionMultiplexer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeProject.ObjectPool;
+
+namespace StackExchange.Redis.Pool
+{
+    public class PooledConnectionMultiplexer : PooledObject
+    {
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
+        public PooledConnectionMultiplexer(ConfigurationOptions config)
+        {
+            this._connectionMultiplexer = ConnectionMultiplexer.Connect(config);
+            this.OnValidateObject += context => this._connectionMultiplexer.IsConnected;
+        }
+
+        public PooledConnectionMultiplexer(string configStr):this(ConfigurationOptions.Parse(configStr))
+        {
+        }
+
+        public ConnectionMultiplexer ConnectionMultiplexer => this._connectionMultiplexer;
+    }
+}
