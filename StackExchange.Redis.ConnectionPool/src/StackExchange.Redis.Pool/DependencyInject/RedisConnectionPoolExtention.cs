bb6496be1457b53c00b09caaeac4f4fa92ad9b49
ULiiAn <chenshenyuan@gznb.com>
2017-10-18 11:30:45 +0800

    add

---
+++ StackExchange.Redis.ConnectionPool/src/StackExchange.Redis.Pool/DependencyInject/RedisConnectionPoolExtention.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CodeProject.ObjectPool;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StackExchange.Redis.ConnectionPool.DependencyInject
+{
+    public static class RedisConnectionPoolExtention
+    {
+        public static void AddRedisConnectionPool(this IServiceCollection serviceCollection, ConfigurationOptions config, int poolSize)
+        {
+            serviceCollection.AddSingleton<ObjectPool<PooledConnectionMultiplexer>>(srv =>
+                new ObjectPool<PooledConnectionMultiplexer>(poolSize, () => new PooledConnectionMultiplexer(config)));
+            serviceCollection.AddScoped<IConnectionMultiplexer>(srv => srv.GetRequiredService<ObjectPool<PooledConnectionMultiplexer>>().GetObject());
+        }
+
+        public static void AddRedisConnectionPool(this IServiceCollection serviceCollection, Action<ConfigurationOptions> configAction, int poolSize)
+        {
+            var config = new ConfigurationOptions();
+            configAction(config);
+            AddRedisConnectionPool(serviceCollection, config, poolSize);
+        }
+
+        public static void AddRedisConnectionPool(this IServiceCollection serviceCollection, string configsStr, int poolSize)
+        {
+            AddRedisConnectionPool(serviceCollection, ConfigurationOptions.Parse(configsStr), poolSize);
+        }
+    }
+}
---
+++ StackExchange.Redis.ConnectionPool/test/StackEcchange.Redis.Pool.Test/PooledConnectionMultiplexerTest.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CodeProject.ObjectPool;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using StackExchange.Redis.ConnectionPool;
+using StackExchange.Redis.ConnectionPool.DependencyInject;
+using Xunit;
+
+namespace StackEcchange.Redis.Pool.Test
+{
+    public class PooledConnectionMultiplexerTest
+    {
+        private IServiceProvider provider;
+
+
+        public PooledConnectionMultiplexerTest()
+        {
+            IServiceCollection collection = new ServiceCollection();
+            collection.AddRedisConnectionPool("localhost:6379", 10);
+           
+            provider = collection.BuildServiceProvider();
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+
+        public void SyncTest(int x)
+        {
+            var conn = provider.GetService<IConnectionMultiplexer>();
+
+            var db = conn.GetDatabase();
+            db.StringSet("test", "test" + x);
+            var value = db.StringGet("test");
+            Assert.True(value.HasValue);
+            Assert.Equal(value.ToString(), "test" + x);
+        }
+
+        [Fact]
+
+        public void ParallelTest()
+        {
+            void RunTest(int x)
+            {
+                var conn = provider.GetService<IConnectionMultiplexer>();
+
+                var db = conn.GetDatabase();
+                db.StringSet("test" + x, "test" + x);
+                var value = db.StringGet("test" + x);
+                Assert.True(value.HasValue);
+                Assert.Equal(value.ToString(), "test" + x);
+            }
+
+            Enumerable.Range(0, 1000).AsParallel().WithDegreeOfParallelism(10).ForAll(RunTest);
+        }
+
+        [Fact]
+
+        public void ParallelTest2()
+        {
+            var pool = this.provider.GetService<ObjectPool<PooledConnectionMultiplexer>>();
+            void RunTest(int x)
+            {
+                using (var conn = pool.GetObject())
+                {
+                    var db = conn.GetDatabase();
+                    db.StringSet("test" + x, "test" + x);
+                    var value = db.StringGet("test" + x);
+                    Assert.True(value.HasValue);
+                    Assert.Equal(value.ToString(), "test" + x);
+                }
+                
+            }
+            Enumerable.Range(0, 1000).AsParallel().WithDegreeOfParallelism(10).ForAll(RunTest);
+        }
+    }
+}
